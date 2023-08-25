e45c2b046b68d8c0a0e5c3638f89dbd7cc86fd35
Kundan Kumar <kundan.k1988@gmail.com>
2023-08-26 00:38:28 +0530

    Login & Forget Password added

--- controllers/controller.js
+++ controllers/controller.js
@@ -20,4 +20,39 @@ const createUser = async (req, res) => {
     }
 }
 
-module.exports = { createUser }
+const login = async (req, res) => {
+    try {
+        const user = await User.findAll({
+            where: { email: req.body.email }
+        })
+        if (user.length == 0) {
+            res.status(401).json({ message: 'User is not registered.' })
+        } else if (user[0].password != req.body.password) {
+            res.status(401).json({ message: 'Password is wrong, Try again with correct password.' })
+        } else if (user[0].status == false) {
+            res.status(401).json({ message: 'User account is not yet activated' })
+        } else if (user[0].password == req.body.password) {
+            res.status(200).json({ message: 'Successfully logged in.. Congratulations...' })
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Something went worng, Please try again later.." })
+    }
+}
+
+const forgetPassword = async (req, res) => {
+    try {
+        const user = await User.findAll({
+            where: { email: req.body.email }
+        })
+        if (user.length == 0) {
+            res.status(401).json({ message: 'User is not registered.' })
+        } else {
+            res.status(200).send(`Your password is ${user[0].password}`)
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: "Something went worng, Please try again later.." })
+    }
+}
+
+module.exports = { createUser, login, forgetPassword }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { createUser } = require('../controllers/controller')
+const { createUser, login, forgetPassword } = require('../controllers/controller')
 
 router.post('/signup', createUser)
+
+router.post('/login', login)
+
+router.post('/forgetPassword', forgetPassword)
 
 module.exports = router
